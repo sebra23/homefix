0f7e695011faa96c1d49f2db0e6937e7e2091c08
sebra23 <sebastian.lilliecreutz@gmail.com>
2026-08-26 20:19:34 +0200

    Set callbackWaitsForEmptyEventLoop to false in Netlify function to prevent pg pool timeouts

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,10 @@
 import serverless from 'serverless-http';
 import { app } from '../../src/server.js';
 
-export const handler = serverless(app);
+const serverlessHandler = serverless(app);
+
+export const handler = async (event, context) => {
+    // Prevent pg connection pool sockets from keeping the event loop active and hanging Lambda
+    context.callbackWaitsForEmptyEventLoop = false;
+    return await serverlessHandler(event, context);
+};
